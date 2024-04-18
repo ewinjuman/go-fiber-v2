a12db69bd800db8becb464a86cb20a72aceddb8e
Herwin Siajuman <herwin.siajuman@ottodigital.id>
2024-04-18 16:09:24 +0700

    convert date

--- pkg/utils/converter.go
+++ pkg/utils/converter.go
@@ -2,9 +2,12 @@ package utils
 
 import (
 	"encoding/json"
+	"fmt"
+	"github.com/pkg/errors"
 	Error "gitlab.pede.id/otto-library/golang/share-pkg/error"
 	"net/http"
 	"strings"
+	"time"
 )
 
 func ObjectToObject(in interface{}, out interface{}) {
@@ -43,5 +46,39 @@ func ConvertPhoneNumber(mobilePhoneNumber string) (newMobilePhoneNumber string, 
 		return
 	}
 
+	return
+}
+
+func ConvertDate(date, format_need string) (result string, err error) {
+	listformats := []string{
+		"02 January 2006",
+		"02 Jan 2006",
+		"02-01-2006",
+		"02/01/2006",
+		"01/02/2006",
+		"2006-01-02",
+		"January 02, 2006",
+		"Jan 02, 2006",
+		"2006-01-02 15:04:05",
+	}
+
+	// Parsing tanggal dengan berbagai format
+	var t time.Time
+
+	for _, format := range listformats {
+		t, err = time.Parse(format, date)
+		if err == nil {
+			break
+		}
+	}
+
+	// Cek apakah parsing berhasil
+	if err != nil {
+		err = errors.New(fmt.Sprintf("Gagal memparsing tanggal: %v", err.Error()))
+		return
+	}
+
+	// Mengonversi tanggal ke format yang diinginkan
+	result = t.Format(format_need)
 	return
 }
